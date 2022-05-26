a0c9412c49dbdebf032bef2cc382bbfd56442888
bobinlee <bobeen.lee@huiseoul.com>
2022-05-26 20:54:16 +0900

    chore: update next version

--- apps/nextjs-app/next.config.js
+++ apps/nextjs-app/next.config.js
@@ -5,6 +5,10 @@ const withNx = require('@nrwl/next/plugins/with-nx');
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  experimental: {
+    runtime: 'nodejs',
+    serverComponents: true,
+  },
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
